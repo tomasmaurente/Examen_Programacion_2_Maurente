b19308feb8e07f02d43565b25b846fb8dc5dc921
tomasmaurente <tommaurente@gmail.com>
2021-08-06 16:49:37 -0300

    Comentarios DIP y OCP en EXeptions

--- src/Library/Exeptions/NoAceptableRewardExeption.cs
+++ src/Library/Exeptions/NoAceptableRewardExeption.cs
@@ -2,6 +2,9 @@ using System;
 /*
     SRP: Esta clase cumple con SRP ya que su unica responsabilidad es definir una nueva excepcion.
 
+    OCP: No se aplica.
+
+    DIP: No se aplica.
 */
 namespace Library
 {
